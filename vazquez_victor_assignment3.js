c6a8276eed00f04048798812bf4a6ada89f4c50a
victorvazco <v_vazquezcobian@yahoo.com>
2014-08-21 21:57:08 -0400

    Array function working

--- vazquez_victor_assignment3.js
+++ vazquez_victor_assignment3.js
@@ -10,6 +10,12 @@
 //Global variables
 var confirmation = "Paying User";
 var word = "User information reviewed and it is as follows for user: ";
+var boolEan = true;
+var levelsArray = ["levelOne", "levelTwo", "levelThree"];
+var hackerAlert = false;
+var cost = 2;
+
+levelsArray[1] = 'special level two';
 
 
 //Procedure
@@ -17,8 +23,10 @@ var word = "User information reviewed and it is as follows for user: ";
 var procedure = function(access){
     
     if (access == "Paying User") {
-        console.log("Access No Ads");    
-    
+            
+    if (hackerAlert === false) {
+        console.log("Access No Ads");
+    }
     } else {
     console.log("Access with Ads and limited levels");    
         
@@ -30,7 +38,7 @@ var procedure = function(access){
 //String Function
 
 var jsonFunctionString = function(json,string){
-    for (var i = 0; i < json.NewUsers2.length; i++){
+    for (var i = 0; i < json.NewUsers2.length; i++){ //array property
         var user = json.NewUsers2[i];
         var stringReturn = (string + user.userNumber + ", User Id: " + user.userID + ". Age:" + user.age  );
         
@@ -40,9 +48,30 @@ var jsonFunctionString = function(json,string){
 };
 
 
+
+
 //Boolean Function
 
-
+var decideFun = function(truth){
+    
+    while (cost < 2 && cost>= 0){
+        
+    if (truth === true ){
+        cost +1;
+    
+        var response = "Upgraded version ";
+    
+    
+        } else {
+        
+        var response1 = "Lesser game graphics ";
+        
+      
+        }
+         return response1;
+    }
+    return response;
+};
 
 
 //Math Function
@@ -51,6 +80,19 @@ var jsonFunctionString = function(json,string){
 
 
 //Array Function
+
+var arrayLevel = function (level){
+    
+    for (var i = 0; i < levelsArray.length; i++) {   //Array property
+    
+    var i = levelsArray;
+    
+    levelsArray.push('Level Four!');//Array method
+    var newLevel = levelsArray[3];
+    };
+    
+    return newLevel;
+};
 
 
 
@@ -61,9 +103,14 @@ var gameAccess = procedure(confirmation);
 
 var callStringJson = jsonFunctionString(json2,word);
 
+var makeDecide = decideFun(boolEan);
+
+var checkLevels = arrayLevel(levelsArray);
+
 //Returned Values
 
 console.log(callStringJson);
 
+console.log(makeDecide + "is what you will play");
 
-
+console.log("Congratulations, you unlocked " + checkLevels + "!")
